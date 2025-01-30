485bf750018558922df26f7f081f637ae53d0b55
Qianyang Wang <116932670+QianyangWang@users.noreply.github.com>
2025-01-30 16:27:16 -0500

    Add files via upload

---
+++ file_templates/lcproj.plt
@@ -0,0 +1,4 @@
+POLLUTANT,hlw,hls,logkoc,logkdoc,cprep,riverflux
+DOC,40,500,0.0,0.0,500000,0
+Chrysene,100,1800,5,5.3,30,1.5
+Napthalene,30,350,3.3,2.4,150,500
---
+++ file_templates/lcproj.sol
@@ -0,0 +1,28 @@
+SOIL,POLLUTANT,fdoc,cbase
+Eutric Cambisols,Chrysene,0,0
+Mollic Gleysols,Chrysene,0,0
+Eutric Fluvisols,Chrysene,0,0
+Luvic Chernozems,Chrysene,0,0
+Albic Luvisols,Chrysene,0,0
+Dystric Histosols,Chrysene,0,0
+Orthic Podzols,Chrysene,0,0
+Mollic Solonetz,Chrysene,0,0
+WATER,Chrysene,0,0
+Eutric Cambisols,Napthalene,0,0
+Mollic Gleysols,Napthalene,0,0
+Eutric Fluvisols,Napthalene,0,0
+Luvic Chernozems,Napthalene,0,0
+Albic Luvisols,Napthalene,0,0
+Dystric Histosols,Napthalene,0,0
+Orthic Podzols,Napthalene,0,0
+Mollic Solonetz,Napthalene,0,0
+WATER,Napthalene,0,0
+Eutric Cambisols,DOC,0.00001,100000
+Mollic Gleysols,DOC,0.002,6000000
+Eutric Fluvisols,DOC,0.002,6000000
+Luvic Chernozems,DOC,0.002,6000000
+Albic Luvisols,DOC,0.0018,5000000
+Dystric Histosols,DOC,0.00048,25000000
+Orthic Podzols,DOC,0.002,8000000
+Mollic Solonetz,DOC,0.002,8000000
+WATER,DOC,0,0
