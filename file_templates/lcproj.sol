a69258a97a247bafd2e9f85f9a1e02aba14f9e50
Qianyang Wang <116932670+QianyangWang@users.noreply.github.com>
2025-07-21 17:17:48 -0400

    Add files via upload

--- file_templates/lcproj.sol
+++ file_templates/lcproj.sol
@@ -1,28 +1,28 @@
-SOIL,POLLUTANT,fdoc,cbase
-Eutric Cambisols,Chrysene,0,0
-Mollic Gleysols,Chrysene,0,0
-Eutric Fluvisols,Chrysene,0,0
-Luvic Chernozems,Chrysene,0,0
-Albic Luvisols,Chrysene,0,0
-Dystric Histosols,Chrysene,0,0
-Orthic Podzols,Chrysene,0,0
-Mollic Solonetz,Chrysene,0,0
-WATER,Chrysene,0,0
-Eutric Cambisols,Napthalene,0,0
-Mollic Gleysols,Napthalene,0,0
-Eutric Fluvisols,Napthalene,0,0
-Luvic Chernozems,Napthalene,0,0
-Albic Luvisols,Napthalene,0,0
-Dystric Histosols,Napthalene,0,0
-Orthic Podzols,Napthalene,0,0
-Mollic Solonetz,Napthalene,0,0
-WATER,Napthalene,0,0
-Eutric Cambisols,DOC,0.00001,100000
-Mollic Gleysols,DOC,0.002,6000000
-Eutric Fluvisols,DOC,0.002,6000000
-Luvic Chernozems,DOC,0.002,6000000
-Albic Luvisols,DOC,0.0018,5000000
-Dystric Histosols,DOC,0.00048,25000000
-Orthic Podzols,DOC,0.002,8000000
-Mollic Solonetz,DOC,0.002,8000000
-WATER,DOC,0,0
+SOIL,POLLUTANT,fdoc,cbase,geoflx
+Eutric Cambisols,Chrysene,0,0,2550
+Mollic Gleysols,Chrysene,0,0,2550
+Eutric Fluvisols,Chrysene,0,0,2550
+Luvic Chernozems,Chrysene,0,0,2550
+Albic Luvisols,Chrysene,0,0,2550
+Dystric Histosols,Chrysene,0,0,2550
+Orthic Podzols,Chrysene,0,0,2550
+Mollic Solonetz,Chrysene,0,0,2550
+WATER,Chrysene,0,0,0
+Eutric Cambisols,Napthalene,0,0,75
+Mollic Gleysols,Napthalene,0,0,75
+Eutric Fluvisols,Napthalene,0,0,75
+Luvic Chernozems,Napthalene,0,0,75
+Albic Luvisols,Napthalene,0,0,75
+Dystric Histosols,Napthalene,0,0,75
+Orthic Podzols,Napthalene,0,0,75
+Mollic Solonetz,Napthalene,0,0,75
+WATER,Napthalene,0,0,0
+Eutric Cambisols,DOC,0.00001,100000,0
+Mollic Gleysols,DOC,0.002,6000000,0
+Eutric Fluvisols,DOC,0.002,6000000,0
+Luvic Chernozems,DOC,0.002,6000000,0
+Albic Luvisols,DOC,0.0018,5000000,0
+Dystric Histosols,DOC,0.00048,25000000,0
+Orthic Podzols,DOC,0.002,8000000,0
+Mollic Solonetz,DOC,0.002,8000000,0
+WATER,DOC,0,0,0
